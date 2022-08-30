5f869a5c250d928c29de9f21898b908ee181ba72
Igor <Sketch_90@mail.ru>
2022-08-30 16:08:55 +0300

    Fixing Bugs and Refactoring Code in React App

--- src/main/java/com/example/dablin/repos/AssignmentRepo.java
+++ src/main/java/com/example/dablin/repos/AssignmentRepo.java
@@ -13,7 +13,7 @@ public interface AssignmentRepo extends JpaRepository<Assignment, Long> {
      Set<Assignment> findByUser (User user);
 
     @Query("select a from Assignment a " +
-            "where a.status = 'Submitted' " +
+            "where (a.status = 'Submitted' and (a.codeReviewer is null or a.codeReviewer = :codeReviewer))" +
             "or a.codeReviewer = :codeReviewer")
     Set<Assignment> findByCodeReviewer(User codeReviewer);
 }
